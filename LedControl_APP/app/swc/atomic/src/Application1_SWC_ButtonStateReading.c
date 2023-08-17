1f73fb269b461ce34cf1fd621acd5046b251ef6c
Madonna Magdy Moussa <madonnamagdymoussa@gmail.com>
2023-08-17 22:13:22 +0300

    Add files via upload

---
+++ LedControl_APP/app/swc/atomic/src/Application1_SWC_ButtonStateReading.c
@@ -0,0 +1,17 @@
+#include "Rte_Application1_SWC_ButtonStateReading.h"
+
+
+
+
+/*
+*Runnable:TaskA_ReadButtonState_Runnable
+*Event Type:periodic
+*DATA ACCESS:
+*SYNCHRONOUS-SERVER-CALL-POINT:  Rte_Call_R_Port_CS_ReadChannel_App1_Dio_ReadChannel(void)
+*/
+void TaskA_ReadButtonState(void){
+
+
+}
+
+
